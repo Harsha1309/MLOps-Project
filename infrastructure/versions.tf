a737696b1e36965cd885117926639b2861b81f20
Harsha1309 <harshyadav1393@gmail.com>
2026-07-23 00:02:39 +0530

    added infra

---
+++ infrastructure/versions.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "eks-sandbox-tfstate-1f9cc70a" # from backend-bootstrap output: state_bucket_name
+    key          = "eks-sandbox/terraform.tfstate"
+    region       = "us-west-2"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
