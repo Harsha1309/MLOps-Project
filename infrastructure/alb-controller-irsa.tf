4204f913b8f5cde1379a2eb9b36c863ded8f0e4a
Harsha1309 <harshyadav1393@gmail.com>
2026-07-25 15:32:01 +0530

    Added traefik ingress and AWS Load balancer

---
+++ infrastructure/alb-controller-irsa.tf
@@ -0,0 +1,36 @@
+data "aws_iam_policy_document" "alb_controller_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_eks_cluster.main.identity[0].oidc[0].issuer, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+resource "aws_iam_role" "alb_controller" {
+  name               = "sandbox-eks-alb-controller"
+  assume_role_policy = data.aws_iam_policy_document.alb_controller_assume.json
+}
+
+resource "aws_iam_policy" "alb_controller" {
+  name   = "AWSLoadBalancerControllerIAMPolicy-sandbox"
+  policy = file("${path.module}/policies/alb-controller-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller" {
+  role       = aws_iam_role.alb_controller.name
+  policy_arn = aws_iam_policy.alb_controller.arn
+}
+
+output "alb_controller_role_arn" {
+  value = aws_iam_role.alb_controller.arn
+}
